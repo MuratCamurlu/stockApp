5cb32e33959a59132a84b88da852ffcb41b43ca3
MuratÇamurlu <mcamurlu1923@gmail.com>
2023-03-16 20:23:39 +0300

    güccel hali

---
+++ src/hooks/useAxios.jsx
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { useSelector } from "react-redux";
+
+const BASE_URL = "https://10001.fullstack.clarusway.com/";
+
+//* Axios Instance for Public API Request
+export const axiosPublic = axios.create({
+  baseURL: BASE_URL,
+});
+
+const useAxios = () => {
+  const { token } = useSelector((state) => state.auth);
+
+  const axiosWithToken = axios.create({
+    baseURL: BASE_URL,
+    headers: { Authorization: `Token ${token}` },
+  });
+
+  return { axiosWithToken };
+};
+
+export default useAxios;
