56bc0011123b90135a8ee1ad82adcb9d5a571a12
MuratÇamurlu <mcamurlu1923@gmail.com>
2023-03-16 20:32:54 +0300

    backend yazımı devam ediyor

--- src/hooks/useAxios.jsx
+++ src/hooks/useAxios.jsx
@@ -1,9 +1,8 @@
 import axios from "axios";
 import { useSelector } from "react-redux";
 
-const BASE_URL = "https://10001.fullstack.clarusway.com/";
+const BASE_URL = "https://14136.fullstack.clarusway.com/";
 
-//* Axios Instance for Public API Request
 export const axiosPublic = axios.create({
   baseURL: BASE_URL,
 });
